24fda5d18e47325e6cfdb674b1f2ef2fb1418b67
SergioChica <sergioanjaci@gmail.com>
2025-02-13 17:33:55 -0500

    refactor(main): Structure of main changed to work with cloud time.
    feat(time_sync): File created to define cloud time.

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import threading
 # import multiprocessing
 from datetime import datetime
 from utils.zk_connector import ZKConnector
+from config.time_sync import getting_date_time
 from controllers.attendance_controller import AttendanceController
 from controllers.device_controller import DeviceController
 
+EXECUTION_TIME = "17:28"
 
 def main():
 
     print("Iniciando recolección de datos...")
     # Crear instancia del conector
+
+    ntp_date, ntp_time = getting_date_time()
+
     connector = ZKConnector()
 
     deviceController = DeviceController(connector)
@@ -22,12 +27,18 @@ def main():
     controller = AttendanceController(connector)
 
     # Definir la fecha específica
-    specific_date = datetime.now()  # Por ejemplo, para el 11 de febrero de 2025
+    specific_date = datetime.strptime(ntp_date, "%Y-%m-%d")  # Por ejemplo, para el 11 de febrero de 2025
 
     # Procesar asistencias
     controller.process_attendance(specific_date)
 
-schedule.every().day.at("07:00").do(main)
+schedule.every().day.at(EXECUTION_TIME).do(main)
 
-while True:       
-    schedule.run_pending()
+while True:    
+    ntp_date , ntp_time = getting_date_time()
+
+    if ntp_time:
+
+        if ntp_time == EXECUTION_TIME:
+            schedule.run_pending()  
+
