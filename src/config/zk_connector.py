96245d8871cc97c5990e9b75d62d360fac5f8f32
AnaLizca07 <analucelylizcanoacerogmail.com>
2025-02-14 16:52:06 -0500

    feat(config): add ZKConnector for device connectivit

---
+++ src/config/zk_connector.py
@@ -0,0 +1,34 @@
+from zk import ZK
+from dotenv import load_dotenv
+import os
+
+class ZKConnector:
+    def __init__(self):
+        load_dotenv()
+        self.zk = ZK(
+            os.getenv('ZK_DEVICE_IP'),
+            port=int(os.getenv('ZK_DEVICE_PORT', '4370')),
+            timeout=int(os.getenv('ZK_DEVICE_TIMEOUT', '5')),
+            password=os.getenv('ZK_DEVICE_PASSWORD', '0')
+        )
+        self.conn = None
+
+    def connect(self):
+        try:
+            if not self.conn:
+                self.conn = self.zk.connect()
+                if self.conn:
+                    self.conn.disable_device()
+            return self.conn
+        except Exception as e:
+            print(f"Error connecting to device: {e}")
+            return None
+
+    def disconnect(self):
+        try:
+            if self.conn:
+                self.conn.enable_device()
+                self.conn.disconnect()
+                self.conn = None
+        except Exception as e:
+            print(f"Error disconnecting: {e}")
