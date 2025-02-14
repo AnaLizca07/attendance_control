ebc3b7b5b259beff47bb719ff41a8cfae4cb3125
AnaLizca07 <analucelylizcanoacerogmail.com>
2025-02-14 07:34:56 -0500

    Fixing time argument

--- main.py
+++ main.py
@@ -9,28 +9,20 @@ from config.time_sync import getting_date_time
 from controllers.attendance_controller import AttendanceController
 from controllers.device_controller import DeviceController
 
-EXECUTION_TIME = "17:28"
+EXECUTION_TIME = "07:28"
 
 def main():
 
     print("Iniciando recolección de datos...")
     # Crear instancia del conector
 
-    ntp_date, ntp_time = getting_date_time()
-
     connector = ZKConnector()
-
-    deviceController = DeviceController(connector)
-    deviceController.get_device_info()
     
     # Crear instancia del controlador
     controller = AttendanceController(connector)
 
-    # Definir la fecha específica
-    specific_date = datetime.strptime(ntp_date, "%Y-%m-%d")  # Por ejemplo, para el 11 de febrero de 2025
-
     # Procesar asistencias
-    controller.process_attendance(specific_date)
+    controller.process_attendance()
 
 schedule.every().day.at(EXECUTION_TIME).do(main)
 
