50bcc082a82ad5074dc170493c7ffa41e6761cb9
SergioChica <sergioanjaci@gmail.com>
2025-02-21 15:41:52 -0500

    refactor(database_config): Changed column structure and functions to insert logs and close the database.

--- src/config/database_config.py
+++ src/config/database_config.py
@@ -1,39 +1,50 @@
+import logging
 import sqlite3
 from pathlib import Path
 
-class DatabaseLogs:
+class DatabaseLogs(logging.Handler):
     def __init__(self, db_folder="data", db_name="attendance_logs.db"):
-        # Define the path to the database in the 'data' folder
+        super().__init__()
+
         self.db_folder = Path(__file__).parent.parent / db_folder
-        self.db_folder.mkdir(parents=True, exist_ok=True)  # Ensures that the folder exists
+        self.db_folder.mkdir(parents=True, exist_ok=True)
         self.db_path = self.db_folder / db_name
 
         self._initialize_db()
 
     def _initialize_db(self):
-        """Create the database if it does not exist."""
+        """Create the table in the database if it does not exist."""
         with sqlite3.connect(self.db_path) as conn:
             cursor = conn.cursor()
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS attendance_logs (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     timestamp TEXT NOT NULL,
-                    logs TEXT NOT NULL,
                     status TEXT NOT NULL,
-                    created_at TEXT NOT NULL,
-                    updated_at TEXT NOT NULL
+                    message TEXT NOT NULL,
+                    logs TEXT NOT NULL,
+                    created_at TEXT NOT NULL
                 )
             """)
             conn.commit()
 
     def emit(self, record):
-        """Inserta un log en la base de datos."""
-        log_message = self.format(record)
-        self.cursor.execute("INSERT INTO logs (level, message) VALUES (%s, %s)", (record.levelname, log_message))
-        self.connection.commit()
-    
+        """Saves the log in the database."""
+        if self.formatter:
+            log_time = self.formatter.formatTime(record, self.formatter.datefmt)
+            log_message = self.format(record)
+        else:
+            log_time = record.created
+            log_message = record.msg
+
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                INSERT INTO attendance_logs (timestamp, status, message, logs, created_at) 
+                VALUES (?, ?, ?, ?, ?)
+            """, (log_time, record.levelname, record.msg, log_message, log_time))
+            conn.commit()
+
     def close(self):
-        """Cierra la conexión con la base de datos."""
-        self.cursor.close()
-        self.connection.close()
+        """Correctly closes the handler."""
         super().close()
